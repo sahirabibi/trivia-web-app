a3e9db60aeadd77f48848195f152847dce4bd288
sahirabibi <sahirabibi96@gmail.com>
2021-08-26 17:40:56 -0400

    Add psuedo code for game logic

--- js/script.js
+++ js/script.js
@@ -1,33 +1,139 @@
 // Global Variables 
 const questions = {};
 
-
-
 // State Variables 
+// url takes three params: category #, difficulty, and type = [easy, medium, hard]
+let api_url =
+	'https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple';
+let paramsObj = {"category": "18", "type": "multiple", "difficulty": "easy"} 
 let currentQuestion = {};
+let remainingQuestions = 10;
 let currentScore = 0;
 
 
 
 // Cached DOM Elements
+// GET START button
 // GET user click on answer choice
 // GET scoreboard to access current score 
+// GET options button
+// GET div containing all answer choices 
+
+// Hide and Show Start Modal
+// grab modal class
+const startModal = document.querySelector("#start-modal")
+// set event DOMonLoad
+window.onload("DOM")
+// grab start button
 
 
-// Functions
 
-function getQuestions() {
+// Classes and Methods
+
+class Data {
     // sends request to API to generate an object containing ten questions and answers
+    constructor(url, paramsObj) {
+        // takes a url and obj of params to modify api request
+        
+    }
+
+    getData() {
+        // stores fetched data
+    }
+
+    formatData() {
+        // formats each question and answer in the data so answer order is different each time
+    }
+
+
 };
 
-function displayQuestion() {
-    // access object and displays a question to user with MC answers
+class Quiz {
+	// access object and displays a question to user with MC answers
+	constructor(data) {
+		// takes formatted data and creates variables for one question
+	}
+
+	displayQuestion(question, answer) {
+        // displays a single question and its choices
+    }
+
+	checkAnswer() {
+		// checks if users choice is correct
+	}
+
 }
 
-function checkAnswer() {
-    // checks if users choice is correct
+class Game {
+    // triggered after each round to check if game is over
+    static isGameOver() {
+        // checks remainingQuestions is greater than 0
+        // if true, triggers playAgain
+
+    }
+
+    static playAgain() {
+        // displays modal asking user to play again and final score
+        // if playAgain 
+            // calls ResetGame
+    }
+
+    static resetGame() {
+        // triggers call to API via Data 
+        // resets score to 0
+        // resets remainingQuestions to 0
+
+    }
+
 }
 
-function updateScore() {
-    // updates scoreboard if user guesses correct choice
-}
+
+
+
+/////////////////// GAME LOGIC ///////////////////////
+// API DATA:
+// "results" : [obj, obj, obj]
+// "category": "science computers"
+// "question": "str"
+// "correct_answer": "str"
+// "incorrect_answer": ["str", "str", "str"]
+
+//PAGE LOAD
+// User sees modal with a brief description of game and a start button 
+// Hits start
+    // Modal toggled to display none
+    // trigger getQuestions() to retrieve data
+
+// Display Question and Answers to User
+// "click" event listener to start button 
+// if clicked
+    // use getQuestions to fetch data from API and store it 
+        // send first obj in array to displayQuestion
+        // remove first obj so the question is not repeated
+    
+    // send data to displayQuestion()
+    // display question will set the current question on the page
+        // set #question to data.question
+        // FOR EACH: set #answers to data.choice
+    // set event listener to each answer choice button
+        // if "click"
+            // sent current choice to checkAnswer();
+                // if target.innerText is same as data.correct_answer
+                    // return true
+                // else return false
+        // if success --> 
+            //change target background color to red 
+            // update score by 1
+            // decrease currentQuestion -1
+        // else 
+            // target.style.backgroundColor = red
+    // display next question
+
+// GAME ENDS
+    // if user clicks Options
+        // display reset button
+        // "click" on reset --> start over
+    // if no more questions left
+        // currentQuestion = 0; 
+
+       
