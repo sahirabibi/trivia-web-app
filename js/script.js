fd4c8d3400c36fe3c31b7cfc301f6baddc905ac4
sahirabibi <sahirabibi96@gmail.com>
2021-08-26 14:35:42 -0400

    Built basic frame for index.html

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// Global Variables 
+const questions = {};
+
+
+
+// State Variables 
+let currentQuestion = {};
+let currentScore = 0;
+
+
+
+// Cached DOM Elements
+// GET user click on answer choice
+// GET scoreboard to access current score 
+
+
+// Functions
+
+function getQuestions() {
+    // sends request to API to generate an object containing ten questions and answers
+};
+
+function displayQuestion() {
+    // accesses object and displays a question to user with MC answers
+}
+
+function checkAnswer() {
+    // checks if users choice is correct
+}
+
+function updateScore() {
+    // updates scoreboard if user guesses correct choice
+}
